65783c6a724ddd6f04cf0ea83aea9041efab5823
longhe <LONGHE001@huazhu.com>
2019-04-10 17:04:31 +0800

    修改provider

---
+++ takephoto/src/main/java/com/longshihan/takephoto/TakePhoteProvide.java
@@ -0,0 +1,14 @@
+package com.longshihan.takephoto;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by LONGHE001.
+ *
+ * @time 2019/4/10 0010
+ * @des
+ * @function
+ */
+
+public class TakePhoteProvide extends FileProvider {
+}
